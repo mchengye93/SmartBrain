629c139b37b9ecdf2884d54154c447e2b4a990ad
Marbo Cheng Ye <marbochengye@gmail.com>
2019-11-12 21:46:34 -0800

    Added Navigation component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Navigation from './Navigation/Nagivation.js';
 
 class App extends Component {
   render() {
     return (
       <div>
         Hello World
+        <Navigation />
       </div>
     );
   }

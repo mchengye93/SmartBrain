cd5fd15b40a59730151a4022cb8b076c7fc5d1a8
Marbo Cheng Ye <marbochengye@gmail.com>
2019-11-12 21:47:31 -0800

    added logo component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navigation from './Navigation/Nagivation.js';
+import Logo from './Logo/Logo.js';
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <div>
         Hello World
         <Navigation />
+        <Logo />
       </div>
     );
   }

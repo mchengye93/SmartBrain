ce3e9185c5f59a3ae88bf8a8162b7606e4e0885f
Marbo Cheng Ye <marbochengye@gmail.com>
2019-11-12 21:44:45 -0800

    add tachyons file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -13,5 +13,7 @@ import ReactDOM from 'react-dom';
 // Import the App component which is the top level component of our app
 import App from './components/App.jsx';
 
+
+
 // Attach App to the DOM, replacing the div with ID app
 ReactDOM.render(<App/>, document.getElementById('app'));

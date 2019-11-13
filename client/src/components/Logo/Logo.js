c1580a9894dbdbdb38b7808e4bf80b40b9e114da
Marbo Cheng Ye <marbochengye@gmail.com>
2019-11-12 22:03:55 -0800

    brain logo added

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const Logo = () => {
-    return (
-        <div>
-           Logo 
+  return (
+    <div className='ma4 mt0'>
+   
+        <div className="Tilt-inner pa3">
+          <img style={{paddingTop: '5px'}} alt='logo' src="https://img.icons8.com/carbon-copy/100/000000/brain.png"/>
         </div>
-    )
+  
+    </div>
+  );
 }
+
 export default Logo;
